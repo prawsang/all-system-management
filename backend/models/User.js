1c69401745df14da94b7d6d499d8e514c6db82c9
Prawsang <prawcha.p@gmail.com>
2019-01-19 23:01:44 +0700

    add user routes and password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,28 @@
 const Sequelize = require("sequelize");
-const db = require('../config/database');
+const db = require("../config/database");
 
-const User = db.define('users',{
-    staff_code: {
-        type: Sequelize.STRING,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING
-    },
-    password: {
-        type: Sequelize.STRING
-    },
-    department: {
-        type: Sequelize.STRING
-    }
+const User = db.define("users", {
+	staff_code: {
+		type: Sequelize.STRING,
+		primaryKey: true
+	},
+	name: {
+		type: Sequelize.STRING
+	},
+	password: {
+		type: Sequelize.STRING
+	},
+	department: {
+		type: Sequelize.STRING
+	}
 });
+
+// Do not query password
+User.prototype.toJSON = function() {
+	const values = Object.assign({}, this.get());
+
+	delete values.password;
+	return values;
+};
 
 module.exports = User;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/User");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+const bcrypt = require('bcrypt');
 
 router.get("/get-all", (req, res) =>
 	User.findAll()
@@ -19,6 +20,63 @@ router.get("/:staff_code", (req, res) => {
 		}
 	})
 		.then(user => res.send(user))
+		.catch(err => res.status(500).send(err));
+});
+
+// Add New User
+router.post("/add", (req, res) => {
+    const { name, staff_code, department } = req.query;
+    let { password } = req.query;
+    bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            User.create({
+                staff_code,
+                name,
+                department,
+                password: hashedPassword
+            })
+                .then(rows => res.sendStatus(200))
+                .catch(err => console.log(err));
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+// Edit User
+router.put("/:staff_code/edit", (req, res) => {
+	const { staff_code } = req.params;
+    const { name, department } = req.query;
+    let { password } = req.query;
+    bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            console.log(password)
+            User.update({
+                name,
+                department,
+                password: hashedPassword
+            },{
+                where: {
+                    staff_code: {
+                        [Op.eq]: staff_code
+                    }
+                }
+            })
+                .then(rows => res.sendStatus(200))
+                .catch(err => res.status(500).send(err));
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+// Delete user
+router.delete("/:staff_code", (req, res) => {
+	const { staff_code } = req.params;
+	User.destroy({
+		where: {
+			staff_code: {
+				[Op.eq]: staff_code
+			}
+		}
+	})
+		.then(rows => res.sendStatus(200))
 		.catch(err => res.status(500).send(err));
 });
 
