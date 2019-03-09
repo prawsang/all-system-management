004722d678430bb1781bea988fd3934cd82acb6e
Prawsang <prawcha.p@gmail.com>
2019-03-09 22:37:01 +0700

    (front): installation without billing

--- client/src/modules/single/pages/withdrawal/index.js
+++ client/src/modules/single/pages/withdrawal/index.js
@@ -155,29 +155,37 @@ class Withdrawal extends React.PureComponent {
 									)}
 								</div>
 								<hr />
-								<div>
-									<h5 className="no-mt has-mb-10">
-										การวางบิล
-										<span
-											className="is-clickable accent has-ml-10 is-6"
-											onClick={() => this.setState({ editBilling: true })}
-										>
-											Edit
-										</span>
-									</h5>
-									<div className="has-mb-10">
-										<span
-											className={
-												!data.withdrawal.billed ? "is-bold danger" : ""
-											}
-										>
-											{data.withdrawal.billed
-												? "วางบิลแล้ว"
-												: "ยังไม่ได้วางบิล"}
-										</span>
-									</div>
-								</div>
-								<hr />
+								{data.withdrawal.type === "INSTALLATION" && (
+									<React.Fragment>
+										<div>
+											<h5 className="no-mt has-mb-10">
+												การวางบิล
+												<span
+													className="is-clickable accent has-ml-10 is-6"
+													onClick={() =>
+														this.setState({ editBilling: true })
+													}
+												>
+													Edit
+												</span>
+											</h5>
+											<div className="has-mb-10">
+												<span
+													className={
+														!data.withdrawal.billed
+															? "is-bold danger"
+															: ""
+													}
+												>
+													{data.withdrawal.billed
+														? "วางบิลแล้ว"
+														: "ยังไม่ได้วางบิล"}
+												</span>
+											</div>
+										</div>
+										<hr />
+									</React.Fragment>
+								)}
 								<div>
 									<h5 className="no-mt has-mb-10">
 										Remarks
